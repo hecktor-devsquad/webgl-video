fab57405de710b9e0f26617aef301032f3bb53b8
Hecktor Viegas <hecktor@devsquad.com>
2025-04-03 08:25:48 -0300

    Refactor Vite configuration for clarity and update base path comment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/webgl-video/', // Deve corresponder ao caminho após o domínio no campo "homepage"
+  base: '/webgl-video/', // Caminho base para GitHub Pages
   plugins: [react()],
-})
+});
