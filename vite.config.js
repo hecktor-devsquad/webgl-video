15704035cd71927174658432844d6a3828d3328a
Hecktor Viegas <hecktor@devsquad.com>
2025-04-03 07:55:22 -0300

    Configure Vite for deployment and set homepage in package.json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/webgl-video/', // Deve corresponder ao caminho após o domínio no campo "homepage"
   plugins: [react()],
 })
